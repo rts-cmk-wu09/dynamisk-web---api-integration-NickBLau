3159431b5c9a135a87de776c79860cc8e2084441
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-03-13 12:05:16 +0100

    moved article from html to javascript

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+let pokemonList = document.querySelector("#pokemon-list")
+
 fetch("https://pokeapi.co/api/v2/pokemon")
 .then((response) => response.json())
-.then((data) => console.log(data));
+.then((data) => {data.results.forEach(result =>{
+    pokemonList.innerHTML +=
+    `        <article>
+    <h2>${result.name}</h2>
+    <img src="pokemon-img" alt="img.alt">
+    <p>${result.url.abilties}</p>
+</article>`
+})
+    console.log(data.results[0])} );
 
+
+ fetch("https://pokeapi.co/api/v2/pokemon/1/")
+ .then((response) => response.json())
+ .then((data) => console.log(data));
