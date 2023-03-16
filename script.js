f2d41650828e83348c01c71a27c887ddfd904988
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-03-16 13:07:48 +0100

    add bug fixes

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let pokemonList = document.querySelector("#pokemon-list")
 
 
-for (let i = 1; i < 11; i++) {
+for (let i = 1; i < 10; i++) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
     .then((response) => response.json())
-    .then((data) =>pokemonList.innerHTML +=
-    `        <article>  
+    .then((data) => pokemonList.innerHTML +=`
+    <article>  
     <p class="dex-entry">#${data.id}</p>
-    <img class="pokemon-image" src="${data.sprites.front_default}" alt=" ">
+    <a href="details.html?name=${data.name}"><img class="pokemon-image" src="${data.sprites.front_default}" alt=" "></a>
     <h2>${data.name}</h2>
-    <p class="dex-types">${data.types.map( type => type.type.name).join(' & ')} </p>
-    
+    <div>
+    <p class="dex-types ${data.types[0].type.name}">
+    ${data.types.map( type => type.type.name).join(' & ')} </p>
+    </div>
 </article>`
-)}
 
+
+)}
