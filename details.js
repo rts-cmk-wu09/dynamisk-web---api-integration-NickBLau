91bd8881d9877ed63bbed231da9bebbb533d65df
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-03-16 10:58:08 +0100

    add movelist to details.js

--- details.js
+++ details.js
@@ -1,43 +1,26 @@
-// const params = new URLSearchParams(window.location.search);
-// const id = params.get("id");
+const params = new URLSearchParams(document.location.search);
+const pokemonname = params.get("name");
 
-// let pokemonDetails = document.querySelector('#pokemon-details')
 
-// fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-//     .then((response) => response.json())
-//     .then((data) => pokemonDetails.innerHTML +=`
-
-// <img src="${data.image}" alt="">
-// `)
-
-// fetch("https://pokeapi.co/api/v2/pokemon/1/")
-// .then((response) => response.json())
-// .then((data) => console.log(data));
-
-const params = new URLSearchParams(window.location.search);
-const newId = params.get("id");
-
-let destinationImage = document.querySelector("#destinationImage")
-let moveSet = document.querySelector("#move-set")
-
-fetch(`https://pokeapi.co/api/v2/pokemon/`)
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonname}`)
 .then(response => response.json())
-.then(data => {document.querySelector('body').innerHTML +=` 
+.then(pokemonData => {
+    console.log(pokemonname);
+    document.querySelector('body').innerHTML +=` 
 
 <section>
-<h2>${data.name}</h2>
+<h2>${pokemonData.forms[0].name}</h2>
+
+
 
 
 </section>`
-console.log(data);
-// ${data.moves.forEach((move)).move.name}
-//     data.moves.forEach(move => { 
-//     moveSet.innerHTML +=`
-//     <p>${move}</p>`
-//     console.log(move[1]);
-// });
 
+pokemonData.moves.forEach(move => {
+    document.querySelector('body').innerHTML +=` 
+    <p >${move.move.name}
+    </p>
+    `;
+console.log(move.move);
+});
 })
-
-{/* <p> Moveset [${data.moves[1].move.name}] </p> */}
-// <p class="move-set">${data.moves.map( move => move.move.name).join('  ')} </p> 
