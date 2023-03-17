eed0de40c8912373879f1f86cfe30d8db1d015e4
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-03-17 14:03:45 +0100

    add span class & data weight/height

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 let pokemonList = document.querySelector("#pokemon-list")
 
+let kantoPokemon = 151;
+const search = document.querySelector("search") 
+const searchInput = document.querySelector("[data-search]")
 
-for (let i = 1; i < 10; i++) {
+
+for (let i = 1; i <= 10; i++) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
     .then((response) => response.json())
     .then((data) => pokemonList.innerHTML +=`
     <article>  
     <p class="dex-entry">#${data.id}</p>
-    <a href="details.html?name=${data.name}"><img class="pokemon-image" src="${data.sprites.front_default}" alt=" "></a>
+    <a href="details.html?name=${data.name}"><img class="pokemon-image ${data.types[0].type.name}" src="${data.sprites.front_default}" alt=" "></a>
     <h2>${data.name}</h2>
-    <div>
-    <p class="dex-types ${data.types[0].type.name}">
-    ${data.types.map( type => type.type.name).join(' & ')} </p>
+
+    <div class="dex-types">
+    ${data.types.map( type => {
+
+        return `<span class="${type.type.name}">${type.type.name}</span>`
+    }).join(" ")}
     </div>
-</article>`
-
-
-)}
+    <div class="weight-height">
+    <p> weight: ${data.weight}  </p>
+    <p> height: ${data.height} </p>
+    <div>
+</article>`)}
